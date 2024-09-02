7d8c5edda650361cc07c82ec4e44046c644c41c1
Orkhanibrahim357 <orkhan_ibrahim@mail.ru>
2024-09-02 14:18:36 +0400

    change bug

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,15 +5,13 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1  # Decrement n to avoid an infinite loop
+        n -= 1
     return result
 
-# Check if a command-line argument is provided
 if len(sys.argv) < 2:
     print("Usage: ./factorial.py <number>")
     sys.exit(1)
 
-# Convert the command-line argument to an integer and calculate the factorial
 try:
     number = int(sys.argv[1])
     f = factorial(number)
