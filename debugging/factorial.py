15be9ef829862593888867713f1ffc897a511e2e
Orkhanibrahim357 <orkhan_ibrahim@mail.ru>
2024-09-02 14:21:07 +0400

    changed bug

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,17 +5,5 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1
+        n -= 1  # Decrement n to avoid an infinite loop
     return result
-
-if len(sys.argv) < 2:
-    print("Usage: ./factorial.py <number>")
-    sys.exit(1)
-
-try:
-    number = int(sys.argv[1])
-    f = factorial(number)
-    print(f)
-except ValueError:
-    print("Please provide a valid integer.")
-    sys.exit(1)
